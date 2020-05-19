50adb09694fb02c9b3c3c4e22da4ca10a5a50613
post@kxe.sakura.ne.jp <post@kxe.sakura.ne.jp>
2020-05-19 22:59:31 +0900

    パワードギア用の設定を追加

--- src/rapidfire/rapidfire.ino
+++ src/rapidfire/rapidfire.ino
@@ -4,7 +4,7 @@
  * Arduino Leonardo 互換ボード
  * 
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version    20200513
+ * @version    20200519
  * 
  * @link       https://github.com/xx20170204xx/ArduinoRapidFire_NEOGEO
  */
@@ -51,6 +51,7 @@
 
 #define DEF_SET_CPS2_DDSOM  0
 #define DEF_SET_CPS2_AVSP   0
+#define DEF_SET_CPS2_PGEAR  0
 #define DEF_SET_S32_DBZVRVS 0
 #define DEF_SET_S32_GA2     0
 #define DEF_SET_DEVTEST01   0
@@ -224,6 +225,32 @@ int g_syncINPin = -1;
     { INPIN_BTN3, OUTPIN_BTN3, RPD_SPD_30, BIND_BTN6,   NULL, 0, 0, },
     { INPIN_BTN4, OUTPIN_BTN4, RPD_SPD_15, BIND_NONE,   NULL, 0, 0, },
     { INPIN_BTN5, OUTPIN_BTN5, RPD_SPD_30, BIND_NONE,   NULL, 0, 0, },
+    { INPIN_BTN6, OUTPIN_BTN6, RPD_SPD_30, BIND_NONE,   NULL, 0, 0, },
+  };
+
+#elif DEF_SET_CPS2_PGEAR == 1
+  /*
+   * パワードギア
+   * ** ボタン配置
+   * [1] [2] [3]
+   * [4] [5] [6]
+   * 
+   * ** 各ボタンの設定
+   * [1] …… ボタン1(Auto有効時 30連)
+   * [2] …… ボタン2(Auto有効時 30連)
+   * [3] …… ボタン3(Auto有効時 30連)
+   * [4] …… マクロ(ボタン1/ボタン2の同時押し)
+   * [5] …… マクロ(ボタン2/ボタン3の同時押し)
+   * [6] …… ボタン1
+  */
+  
+  SBTNINFO g_BtnInfo[BTN] = {
+    /*InputPin    OutputPin    Timing      Bind　　　　　MacroFunc  -  - */
+    { INPIN_BTN1, OUTPIN_BTN1, RPD_SPD_30, BIND_BTN6,   NULL, 0, 0, },
+    { INPIN_BTN2, OUTPIN_BTN2, RPD_SPD_30, BIND_NONE,   NULL, 0, 0, },
+    { INPIN_BTN3, OUTPIN_BTN3, RPD_SPD_30, BIND_NONE,   NULL, 0, 0, },
+    { INPIN_BTN4, OUTPIN_BTN4, RPD_SPD_15, BIND_NONE,   oneStep_AnB_Macro, 0, 0, },
+    { INPIN_BTN5, OUTPIN_BTN5, RPD_SPD_30, BIND_NONE,   oneStep_BnC_Macro, 0, 0, },
     { INPIN_BTN6, OUTPIN_BTN6, RPD_SPD_30, BIND_NONE,   NULL, 0, 0, },
   };
 
