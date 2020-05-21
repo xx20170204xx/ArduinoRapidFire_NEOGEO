ad93968423ec366bbb41ee29cc3a7bcbc6d9bf64
post@kxe.sakura.ne.jp <post@kxe.sakura.ne.jp>
2020-05-21 11:43:39 +0900

    同時押しのパターンを追加

--- src/rapidfire/rapidfire.ino
+++ src/rapidfire/rapidfire.ino
@@ -167,7 +167,15 @@ void oneStep_DBZ_VRVS_Macro2(void);
 
 void oneStep_AnB_Macro(void);
 void oneStep_BnC_Macro(void);
+void oneStep_CnD_Macro(void);
+void oneStep_DnE_Macro(void);
+void oneStep_EnF_Macro(void);
+void oneStep_AnD_Macro(void);
+void oneStep_BnE_Macro(void);
+void oneStep_CnF_Macro(void);
+void oneStep_AnC_Macro(void);
 void oneStep_AnBnC_Macro(void);
+void oneStep_DnEnF_Macro(void);
 /******************************************************************************/
 int g_autoPin =  INPIN_AUTO;
 int g_clearPin = INPIN_CLEAR;
@@ -800,6 +808,90 @@ void oneStep_BnC_Macro(void)
 } /* oneStep_BnC_Macro */
 /******************************************************************************/
 /*
+ * oneStep_CnD_Macro
+ * 
+ * ボタン3/ボタン4の同時押しボタン
+ * 
+*/
+void oneStep_CnD_Macro(void)
+{
+  g_BtnInfo[2].outStatus = LOW;
+  g_BtnInfo[3].outStatus = LOW;
+} /* oneStep_CnD_Macro */
+/******************************************************************************/
+/*
+ * oneStep_DnE_Macro
+ * 
+ * ボタン4/ボタン5の同時押しボタン
+ * 
+*/
+void oneStep_DnE_Macro(void)
+{
+  g_BtnInfo[3].outStatus = LOW;
+  g_BtnInfo[4].outStatus = LOW;
+} /* oneStep_DnE_Macro */
+/******************************************************************************/
+/*
+ * oneStep_EnF_Macro
+ * 
+ * ボタン5/ボタン6の同時押しボタン
+ * 
+*/
+void oneStep_EnF_Macro(void)
+{
+  g_BtnInfo[4].outStatus = LOW;
+  g_BtnInfo[5].outStatus = LOW;
+} /* oneStep_EnF_Macro */
+/******************************************************************************/
+/*
+ * oneStep_AnD_Macro
+ * 
+ * ボタン1/ボタン4の同時押しボタン
+ * 
+*/
+void oneStep_AnD_Macro(void)
+{
+  g_BtnInfo[0].outStatus = LOW;
+  g_BtnInfo[3].outStatus = LOW;
+} /* oneStep_AnD_Macro */
+/******************************************************************************/
+/*
+ * oneStep_BnE_Macro
+ * 
+ * ボタン2/ボタン5の同時押しボタン
+ * 
+*/
+void oneStep_BnE_Macro(void)
+{
+  g_BtnInfo[1].outStatus = LOW;
+  g_BtnInfo[4].outStatus = LOW;
+} /* oneStep_BnE_Macro */
+/******************************************************************************/
+/*
+ * oneStep_CnF_Macro
+ * 
+ * ボタン3/ボタン6の同時押しボタン
+ * 
+*/
+void oneStep_CnF_Macro(void)
+{
+  g_BtnInfo[2].outStatus = LOW;
+  g_BtnInfo[5].outStatus = LOW;
+} /* oneStep_CnF_Macro */
+/******************************************************************************/
+/*
+ * oneStep_AnC_Macro
+ * 
+ * ボタン1/ボタン3の同時押しボタン
+ * 
+*/
+void oneStep_AnC_Macro(void)
+{
+  g_BtnInfo[0].outStatus = LOW;
+  g_BtnInfo[2].outStatus = LOW;
+} /* oneStep_AnC_Macro */
+/******************************************************************************/
+/*
  * oneStep_AnBnC_Macro
  * 
  * ボタン1/ボタン2/ボタン3の同時押しボタン
@@ -811,4 +903,17 @@ void oneStep_AnBnC_Macro(void)
   g_BtnInfo[1].outStatus = LOW;
   g_BtnInfo[2].outStatus = LOW;
 } /* oneStep_AnBnC_Macro */
+/******************************************************************************/
+/*
+ * oneStep_DnEnF_Macro
+ * 
+ * ボタン4/ボタン5/ボタン6の同時押しボタン
+ * 
+*/
+void oneStep_DnEnF_Macro(void)
+{
+  g_BtnInfo[3].outStatus = LOW;
+  g_BtnInfo[4].outStatus = LOW;
+  g_BtnInfo[5].outStatus = LOW;
+} /* oneStep_DnEnF_Macro */
 /******************************************************************************/
